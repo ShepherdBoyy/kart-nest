86914b48e59015571fe57947c20921032261b511
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-09-12 07:09:12 +0800

    Finalize the design of the login page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -20,17 +20,17 @@ export default function Welcome({ user }) {
                             <div className='flex items-center justify-between'>
                                 <div className='flex items-start'>
                                     <div className='flex items-center h-5'>
-                                        <input type='checkbox'aria-describedby='remember' name='remember' id='remember' className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800' />
+                                        <input type='checkbox'aria-describedby='remember' name='remember' id='remember' className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800'/>
                                     </div>
                                     <div className='ml-3 text-sm'>
                                         <label for="remember" className='text-gray-500 dark:text-gray-300'>Remember me</label>
                                     </div>
                                 </div>
-                                <Link href="#">Forgot password?</Link>
+                                <Link href="#" className='text-sm font-medium text-blue-600 hover:underline'>Forgot password?</Link>
                             </div>
-                            <button>Sign in</button>
-                            <p>
-                                Don't have an account yet? <Link href="#">Sign up</Link>
+                            <button type='submit' className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Sign in</button>
+                            <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                                Don't have an account yet? <Link href="#" className='font-medium text-blue-600 hover:underline'>Sign up</Link>
                             </p>
                         </form>
                     </div>

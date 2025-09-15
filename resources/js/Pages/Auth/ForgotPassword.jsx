ba92374463af7931812b9fef4af2c4db37e36d13
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-09-15 11:59:21 +0800

    Add back to login page in forgot password page

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import { Head, useForm } from '@inertiajs/react'
+import { Head, Link, useForm } from '@inertiajs/react'
 import React from 'react'
 
 export default function ForgotPassword({ status }) {
@@ -48,6 +48,10 @@ export default function ForgotPassword({ status }) {
                             </p>
 
                             <button disabled={processing} className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 mt-5 py-2.5 text-center'>Send Reset Password Link</button>
+                            
+                            <p className='text-sm font-light mt-4 text-right text-gray-500 dark:text-gray-400'>
+                                <Link href={route("login")} className='font-medium text-blue-600 hover:underline'>Go back to login</Link>
+                            </p>
                         </div>
                     </form>
                 </div>

5c6c80597c108ab6ee8398d10e144284762e8508
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-09-17 16:44:09 +0800

    Fix issue in dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,20 @@
+import { Head } from '@inertiajs/react'
 import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div>Welcome to Dashboard</div>
+    <>
+      <Head title="Dashboard" />
+
+      <div className='min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900'>
+        <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+          <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+            <h1 className='text-xl text-center font-bold leading-tight tracking-tight md:text-2xl dark:text-white'>
+              Welcome! You're Logged In!
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }

33ede7bb2c11cce697584a9609ddab4b7e9493d6
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-09-24 09:36:02 +0800

    Configure project

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react'
+import React from 'react'
+
+export default function Welcome() {
+  return (
+    <div>
+        <Head title='Welcome' />
+
+        <h1>Welcome to my app</h1>
+    </div>
+  )
+}

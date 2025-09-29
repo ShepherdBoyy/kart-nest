3d963133e697929a1864d375d61924cfd29c55b6
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-09-29 16:38:39 +0800

    Fix issue with tailwind

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,11 @@
+import { Head } from '@inertiajs/react'
+
+export default function Welcome() {
+  return (
+    <>
+        <Head title='Welcome' />
+
+        <div className="bg-red-500 text-white p-4">Tailwind test</div>
+    </>
+  )
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <title inertia>{{ config('app.name', 'Laravel') }} </title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     @routes
     @viteReactRefresh
